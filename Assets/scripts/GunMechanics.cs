0063011d24d46fd15a822dd278751b968ab13be5
nielscoopmans <nielscoopmans@gmail.com>
2024-05-08 23:34:52 +0200

    start of gun mechanics

---
+++ Assets/scripts/GunMechanics.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform Bullet;
+    public Transform BulletHolder;
+    public float BulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((OVRInput.Get(OVRInput.RawButton.RIndexTrigger))){
+            Transform BulletTrans = Instantiate(Bullet,BulletHolder.position,BulletHolder.rotation);
+            Rigidbody BulletRB = BulletTrans.GetComponent<Rigidbody>();
+            BulletRB.AddRelativeForce(Vector3.forward*BulletSpeed);
+        } 
+    }
+}
